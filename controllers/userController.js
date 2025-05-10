d4a7e19a9941cfda4a4bbf4fa900ad9d814fcdfc
aryanrao001 <portfoliorajatyadav@gmail.com>
2025-05-10 18:25:45 +0530

    upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 import validator from 'validator';  // Correct import from validator package
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';  // Correct path to model
 
